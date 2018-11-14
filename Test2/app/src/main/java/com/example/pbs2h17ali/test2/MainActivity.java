05094a466a30b096458d6895063656a5bcbd87e4
Nam-Max Liebner / PBS2H17A <nam-max.liebner@edu.bib.de>
2018-11-14 15:03:11 +0100

    Test

---
+++ Test2/app/src/main/java/com/example/pbs2h17ali/test2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pbs2h17ali.test2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
